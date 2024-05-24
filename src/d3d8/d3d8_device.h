AlpyneDreams/d8vk

[d3d8] EndScene on Reset
Should fix #111

src/d3d8/d3d8_device.h
@@ -817,6 +817,8 @@ namespace dxvk {
     inline bool ShouldRecord() { return m_recorder != nullptr; }
 
     inline void ResetState() {
+      // Resetting implicitly ends scenes started by BeginScene
+      m_bridge->ForceEndScene();
       // Purge cached objects
       // TODO: Some functions may need to be called here (e.g. SetTexture, etc.)
       // in case Reset can be recorded by state blocks and other things.

flibitijibibo/dxvk

src/d3d8/d3d8_device.h
@@ -816,6 +816,8 @@ namespace dxvk {
     inline bool ShouldRecord() { return m_recorder != nullptr; }
 
     inline void ResetState() {
+      // Resetting implicitly ends scenes started by BeginScene
+      m_bridge->EndScene();
       // Purge cached objects
       // TODO: Some functions may need to be called here (e.g. SetTexture, etc.)
       // in case Reset can be recorded by state blocks and other things.