AlpyneDreams/d8vk

[d3d8] Fix extra refs to bridges and textures

src/d3d8/d3d8_device.h
@@ -897,7 +897,7 @@ namespace dxvk {
 
   private:
 
-    D3D9Bridge*           m_bridge;
+    Com<D3D9Bridge>       m_bridge;
     const D3D8Options&    m_d3d8Options;          
 
     Com<D3D8InterfaceEx>  m_parent;

src/d3d8/d3d8_interface.h
@@ -157,7 +157,7 @@ namespace dxvk {
     std::vector<std::vector<d3d9::D3DDISPLAYMODE>>  m_adapterModes;
 
     d3d9::IDirect3D9Ex*                             m_d3d9ex;
-    D3D9InterfaceBridge*                            m_bridge;
+    Com<D3D9InterfaceBridge>                        m_bridge;
     D3D8Options                                     m_d3d8Options;
   };
 

flibitijibibo/dxvk

src/d3d8/d3d8_device.h
@@ -896,7 +896,8 @@ namespace dxvk {
 
   private:
 
-    IDxvkD3D8Bridge*      m_bridge;
+    Com<IDxvkD3D8Bridge>  m_bridge;
+    const D3D8Options&    m_d3d8Options;
 
     Com<D3D8InterfaceEx>  m_parent;
 

src/d3d8/d3d8_interface.h
@@ -157,7 +157,7 @@ namespace dxvk {
     std::vector<std::vector<d3d9::D3DDISPLAYMODE>>  m_adapterModes;
 
     d3d9::IDirect3D9Ex*                             m_d3d9ex;
-    IDxvkD3D8InterfaceBridge*                       m_bridge;
+    Com<IDxvkD3D8InterfaceBridge>                   m_bridge;
     D3D8Options                                     m_d3d8Options;
   };
 